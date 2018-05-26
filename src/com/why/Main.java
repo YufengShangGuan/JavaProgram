8baa6cf51088016f1f08a34398995cce9b8edf6f
YufengShangGuan <why170018@163.com>
2018-05-26 20:45:19 +0800

    Initial commit

---
+++ src/com/why/Main.java
@@ -0,0 +1,13 @@
+package com.why;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for(int i=0;i<6;i++)
+        {
+            System.out.println("Welcome to Java!");
+        }
+
+    }
+}
